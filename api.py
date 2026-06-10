d72bbd5355cd65a150da90d8333a736c7bcbd119
ProgrammerRez <programmerrez2318@gmail.com>
2026-06-10 16:55:26 +0800

    Created API plan

--- api.py
+++ api.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI
-from uvicorn import uv
-
-
-app = FastAPI()
+import uvicorn as uv
 
 
 # Creating a test api for now to test scalability and latency along with api costs
+app = FastAPI()
+
+
+# 1. /research endpoint will run the workflow
+# 2. /json return value will return json file
+# 3. /logs will show logs
+# 4. /file returns markdown of research
+# 5. /costs returns current sessions costs
+
 
 
